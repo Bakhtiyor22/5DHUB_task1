d448c04f5946dbd8325285735d5d8087a80f0936
Bakhtiyor22 <baxtiyorabduraximov2203@gmail.com>
2025-05-12 19:56:39 +0500

    feat: Correctly configure eureka server and docker-compose

--- company-service/src/main/java/org/example/companyservice/CompanyServiceApplication.java
+++ company-service/src/main/java/org/example/companyservice/CompanyServiceApplication.java
@@ -2,25 +2,11 @@ package org.example.companyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
 public class CompanyServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompanyServiceApplication.class, args);
 	}
-
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
-
 }
